2db1b1b7cd132616ffc852ba9848839a15cbd12c
Jero Ginaca <108022277+jeroginaca@users.noreply.github.com>
2022-11-16 18:00:33 +0100

    fist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import react, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("")
+
+function handleSubmit(e){
+  e.preventDefault()
+
+  const newTodo = {
+    id: new Date().getTime(),
+    text: todo,
+    completed: false,
+  }
+
+  setTodos([...todos].concat(newTodo))
+  setTodo("")
+
+  
+}
+
+function deleteTodo(id){
+  const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+  setTodos(updatedTodos)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo list</h1>
+      <form onSubmit={handleSubmit}>
+      <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
+      <button>Add Todo</button>
+      </form>
+     
+      {todos.map((todo) => <div className='todo' key={todo.id}>
+      <div className='textoDeLaTarea'>{todo.text}</div>
+      <button onClick={() => deleteTodo(todo.id)}><i class="fa-solid fa-circle-xmark"></i></button>
+      </div>)}
+     
     </div>
   );
 }
